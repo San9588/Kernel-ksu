ecca6947ef8e05118251a8ca3d9c56e1ef870b8d
Greg Kroah-Hartman <gregkh@google.com>
2022-04-27 13:36:01 +0200

    Merge 4.14.277 into android-4.14-stable
    Changes in 4.14.277
    	etherdevice: Adjust ether_addr* prototypes to silence -Wstringop-overead
    	mm: page_alloc: fix building error on -Werror=array-compare
    	tracing: Have traceon and traceoff trigger honor the instance
    	tracing: Dump stacktrace trigger to the corresponding instance
    	can: usb_8dev: usb_8dev_start_xmit(): fix double dev_kfree_skb() in error path
    	gfs2: assign rgrp glock before compute_bitstructs
    	ALSA: usb-audio: Clear MIDI port active flag after draining
    	tcp: fix race condition when creating child sockets from syncookies
    	tcp: Fix potential use-after-free due to double kfree()
    	dmaengine: imx-sdma: Fix error checking in sdma_event_remap
    	net/packet: fix packet_sock xmit return value checking
    	netlink: reset network and mac headers in netlink_dump()
    	ARM: vexpress/spc: Avoid negative array index when !SMP
    	platform/x86: samsung-laptop: Fix an unsigned comparison which can never be negative
    	ALSA: usb-audio: Fix undefined behavior due to shift overflowing the constant
    	vxlan: fix error return code in vxlan_fdb_append
    	cifs: Check the IOCB_DIRECT flag, not O_DIRECT
    	brcmfmac: sdio: Fix undefined behavior due to shift overflowing the constant
    	drm/msm/mdp5: check the return of kzalloc()
    	net: macb: Restart tx only if queue pointer is lagging
    	stat: fix inconsistency between struct stat and struct compat_stat
    	ata: pata_marvell: Check the 'bmdma_addr' beforing reading
    	dma: at_xdmac: fix a missing check on list iterator
    	powerpc/perf: Fix power9 event alternatives
    	openvswitch: fix OOB access in reserve_sfa_size()
    	ASoC: soc-dapm: fix two incorrect uses of list iterator
    	e1000e: Fix possible overflow in LTR decoding
    	ARC: entry: fix syscall_trace_exit argument
    	ext4: fix symlink file size not match to file content
    	ext4: limit length to bitmap_maxbytes - blocksize in punch_hole
    	ext4: fix overhead calculation to account for the reserved gdt blocks
    	ext4: force overhead calculation if the s_overhead_cluster makes no sense
    	staging: ion: Prevent incorrect reference counting behavour
    	block/compat_ioctl: fix range check in BLKGETSIZE
    	ax25: add refcount in ax25_dev to avoid UAF bugs
    	ax25: fix reference count leaks of ax25_dev
    	ax25: fix UAF bugs of net_device caused by rebinding operation
    	ax25: Fix refcount leaks caused by ax25_cb_del()
    	ax25: fix UAF bug in ax25_send_control()
    	ax25: fix NPD bug in ax25_disconnect
    	ax25: Fix NULL pointer dereferences in ax25 timers
    	ax25: Fix UAF bugs in ax25 timers
    	Revert "net: micrel: fix KS8851_MLL Kconfig"
    	Linux 4.14.277
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I8df77df882183aec177fa9d6d0f8cbe8ebbf54c2

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -217,8 +217,9 @@ static void *ion_buffer_kmap_get(struct ion_buffer *buffer)
 	void *vaddr;
 
 	if (buffer->kmap_cnt) {
-                if (buffer->kmap_cnt == INT_MAX)
+		if (buffer->kmap_cnt == INT_MAX)
 			return ERR_PTR(-EOVERFLOW);
+
 		buffer->kmap_cnt++;
 		return buffer->vaddr;
 	}
